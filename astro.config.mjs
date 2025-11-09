8a6f2462615f9bf12be8b5fa74482d5dc56198d1
hiphopvijay42 <hiphopvijay42@gmail.com>
2025-11-09 07:42:55 +0000

    Changes before Firebase Studio auto-run

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,14 +7,20 @@ import svelte from '@astrojs/svelte';
 import vue from '@astrojs/vue';
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	// Enable many frameworks to support all different kinds of components.
-	integrations: [
-		preact({ include: ['**/preact/*'] }),
-		solid({ include: ['**/solid/*'] }),
-		react({ include: ['**/react/*'] }),
-		svelte(),
-		vue(),
+  // Enable many frameworks to support all different kinds of components.
+  integrations: [
+      preact({ include: ['**/preact/*'] }),
+      solid({ include: ['**/solid/*'] }),
+      react({ include: ['**/react/*'] }),
+      svelte(),
+      vue(),
 	],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
